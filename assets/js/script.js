c304ff21a29af30ff7aba73d2763af1431d99bce
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2022-09-24 22:40:25 -0300

    Hero section

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const mobileNavBar = () =>{
+    let nav = document.querySelector('.nav__links')
+    nav.classList.toggle('open')
+    let btn = document.querySelector('.mobile__menu')
+    btn.classList.toggle('open')
+}
